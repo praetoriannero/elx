10ca27b720a7a842997b2af0ae14984e954fac11
Nicholas Heaven <nick.heaven0930@gmail.com>
2026-04-15 23:07:48 -0500

    stubs for hashtable

---
+++ src/hashtable.c
@@ -0,0 +1,9 @@
+// https://benhoyt.com/writings/hash-table-in-c/
+// refer to the above
+//
+//
+#include "hashtable.h"
+
+static inline void nothing(void) {
+
+}
---
+++ src/hashtable.h
@@ -0,0 +1,7 @@
+// https://benhoyt.com/writings/hash-table-in-c/
+// refer to the above
+//
+//
+#pragma once
+
+static inline void nothing(void);
