cf28443366a813dc8fb88cfa22a4fff96b79b5a9
Nicholas Heaven <nick.heaven0930@gmail.com>
2026-04-17 20:44:54 -0500

    templating out hashtable

--- src/hashtable.c
+++ src/hashtable.c
@@ -1,9 +1,13 @@
 // https://benhoyt.com/writings/hash-table-in-c/
 // refer to the above
-//
-//
 #include "hashtable.h"
 
-static inline void nothing(void) {
+HashTable* hashtable_new(Allocator* alloc, HashFunc hash_func, KeyEqualFunc hash_comp);
 
-}
+void hashtable_init(HashTable* table, Allocator* alloc);
+
+void* hashtable_add(HashTable* table, void* key, void* value);
+
+void* hashtable_remove(HashTable* table, void* key);
+
+void* hashtable_get(HashTable* table, void* key);
--- src/hashtable.h
+++ src/hashtable.h
@@ -1,7 +1,56 @@
 // https://benhoyt.com/writings/hash-table-in-c/
 // refer to the above
-//
-//
 #pragma once
 
-static inline void nothing(void);
+#include "allocator.h"
+#include "modint.h"
+#include "vector.h"
+
+typedef u64 (*HashFunc)(const void* value);
+
+typedef u64 (*KeyEqualFunc)(const void* lhs, const void* rhs);
+
+typedef struct {
+  Vector keys;
+  Vector values;
+  HashFunc hash_func;
+  KeyEqualFunc comp_func;
+} HashTable;
+
+typedef struct {
+  HashTable* table;
+  usize idx;
+} HashTableIter;
+
+typedef struct {
+  HashTable* table;
+  usize idx;
+} HashTableKeyIter;
+
+typedef struct {
+  HashTable* table;
+  usize idx;
+} HashTableValueIter;
+
+HashTable* hashtable_new(Allocator* alloc, HashFunc hash_func, KeyEqualFunc hash_comp);
+
+void hashtable_init(HashTable* table, Allocator* alloc);
+
+void* hashtable_add(HashTable* table, void* key, void* value);
+
+void* hashtable_remove(HashTable* table, void* key);
+
+void* hashtable_get(HashTable* table, void* key);
+
+void hashtable_iter_init(HashTableIter* table_iter, HashTable* table);
+
+void hashtable_iter_next(HashTableIter* table_iter, void** key, void** value);
+
+void hashtable_key_iter_init(HashTableKeyIter* table_iter, HashTable* table);
+
+void hashtable_key_iter_next(HashTableKeyIter* table_iter, void** key);
+
+void hashtable_value_iter_init(HashTableValueIter* table_iter, HashTable* table);
+
+void hashtable_value_iter_next(HashTableValueIter* table_iter, void** value);
+
