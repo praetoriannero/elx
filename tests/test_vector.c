57584510eb4b458d45800f2c4f2ec50a36fb012c
Nicholas Heaven <nick.heaven0930@gmail.com>
2026-04-26 14:57:44 -0500

    vector tests

--- tests/test_vector.c
+++ tests/test_vector.c
@@ -53,8 +53,45 @@ void test_vector_init_with_capacity(void) {
   TEST_ASSERT_TRUE(vec.capacity == 26);
 }
 
+void test_vector_get(void) {
+  Allocator alloc = {};
+  allocator_init(&alloc);
+
+  Vector vec = {};
+  vector_init(&vec, &alloc, sizeof(u64), 13, NULL);
+  
+  u64 t = 3;
+  for (usize idx = 0; idx < 15; idx++) {
+    vector_push(&vec, &t);
+  }
+
+  TEST_ASSERT_TRUE(vector_get(&vec, u64, 4) == 3);
+}
+
+void test_vector_insert(void) {
+  Allocator alloc = {};
+  allocator_init(&alloc);
+
+  Vector vec = {};
+  vector_init(&vec, &alloc, sizeof(u64), 13, NULL);
+  
+  u64 t = 3;
+  for (usize idx = 0; idx < 15; idx++) {
+    vector_push(&vec, &t);
+  }
+
+  t = 7;
+  vector_insert(&vec, 5, &t);
+  
+  TEST_ASSERT_TRUE(vector_get(&vec, u64, 5) == 7);
+}
+
 int main(void) {
 	UNITY_BEGIN();
 	RUN_TEST(test_vector_init);
+	RUN_TEST(test_vector_init_with_capacity);
+	RUN_TEST(test_vector_push);
+	RUN_TEST(test_vector_get);
+	RUN_TEST(test_vector_insert);
 	return UNITY_END();
 }
